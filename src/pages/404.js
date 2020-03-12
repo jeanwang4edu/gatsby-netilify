f21c64a3a93ad99db97bcc610195d0a4a288d8bc
Jean Wang <jeanharmsen@yahoo.com>
2020-03-12 12:56:14 -0400

    add 404 notFound page

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from "react";
+import PrimaryLayout from "../layouts/PrimaryLayout";
+
+const NotFoundPage = () => (
+    <PrimaryLayout column="col-xs-6">
+        <h1>NOT FOUND</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </PrimaryLayout>
+);
+
+export default NotFoundPage;
