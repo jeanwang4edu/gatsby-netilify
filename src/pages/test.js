2c135afd1903867e7d0dbd3215e6f0f3d902ec68
jeanwang2dev <jeanharmsen@yahoo.com>
2022-01-13 11:26:46 -0500

    update package.json and update test.js

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {navigate} from 'gatsby';
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { gsap } from "gsap";
 
-const Test = () => (
+export default function Test() {
+    const listOne = React.createRef();
+
+    useEffect(  
+        () => {
+        gsap.to( listOne.current, { rotation: "+=360" })
+    }, [listOne]);
+    
+    return (
     <div>
-        <Header />
-        <ul>
-        <li>This is an item</li>
-        <li>This is another item</li>
-        <button className="btn" onClick={()=>navigate("/")}>Navigate to Home</button>
-        </ul>
-        <Footer />
-    </div>
-);
-
-export default Test;
+          <Header />
+            
+            <ul>
+                <li ref={listOne}>This is an item</li>
+                <li>This is another item</li>
+            </ul>
+            <button className="btn" onClick={()=>navigate("/")}>Navigate to Home</button>
+          <Footer />
+      </div>
+   );
+}
